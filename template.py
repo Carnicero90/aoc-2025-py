54911d3feaa96eb7350856cc15f9cf2c00ad686a
calavera <filomontani@icloud.com>
2025-12-06 13:15:26 +0100

    boilerplating

---
+++ template.py
@@ -0,0 +1,19 @@
+import sys
+
+filename = sys.argv[1] if len(sys.argv) > 1 else "in"
+
+with open(filename) as file:
+    pass
+
+
+def part1():
+    pass
+
+
+def part2():
+    pass
+
+
+if __name__ == "__main__":
+    print(part1())
+    print(part2())
